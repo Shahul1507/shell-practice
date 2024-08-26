3a1b31583effb14e3d60576ad8c7c1904f3d2e71
shahul <shahul.met@gmil.com>
2024-08-26 18:33:16 +0530

    echo Fruits

--- 06-array.sh
+++ 06-array.sh
@@ -3,9 +3,7 @@
 FRUITS=("APPLE" "ORANGE" "KIWI") # array
 
 echo "First fruit is: ${FRUITS{0}}"
-
 echo "second fruit is: ${FRUITS{1}}"
-
 echo "Third fruit is: ${FRUITS{2}}"
 
 echo "All fruits is: ${FRUITS{@}}"
