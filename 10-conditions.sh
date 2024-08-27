404f03f36eec8f3af06bb7d46b8825814d93a77d
shahul <shahul.met@gmil.com>
2024-08-27 16:55:20 +0530

    conditions with greater or lesser numbers

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,12 +2,11 @@
 
 NUMBER=$1
 
-if [ $NUMBER -ge -gt 20 ] #-gt, -lt, -eq, -ne, -ge, -le
+if [ $NUMBER -gt 20 | -ge 20 ] #-gt, -lt, -eq, -ne, -ge, -le
 
 then
     echo "Given number: $NUMBER is greater than 20"
 else 
-    echo "Given Number: $NUMBER is greter than equal to 20"    
-else
-    echo "Given number: $NUMBER is less than 20"
+    echo "Given Number: $NUMBER is equal to 20"   
+
 fi
