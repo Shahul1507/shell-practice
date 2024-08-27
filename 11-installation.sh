4a3d0ac5ba1cfbb607dce6fb669464af393360ef
shahul <shahul.met@gmil.com>
2024-08-27 18:12:21 +0530

    installation

--- 11-installation.sh
+++ 11-installation.sh
@@ -9,3 +9,17 @@ then
     echo "Please run this script with root priveleges"
     exit 1
 fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y # here installing
+    if [ $? -ne 0 ]
+    then
+        echo "Git installation is not success...check it"
+        exit 1
+    else
+        echo "Git installation is success"
+    fi
